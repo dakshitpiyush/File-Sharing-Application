4b6ccb2777173f4efb1fb8f554ae7bcb55bce456
Dakshit Mahadeshwar <dakshitdm@gmail.com>
2020-11-02 14:57:36 +0530

    Initial commit

---
+++ app/src/main/java/com/dakshit/file_sharing/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dakshit.file_sharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

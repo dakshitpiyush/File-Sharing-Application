0e2bd6dadeed8d7afc3994f196d0cb28296465ee
Dakshit Mahadeshwar <dakshitdm@gmail.com>
2020-11-05 10:29:13 +0530

    basic listView done

--- app/src/main/java/com/dakshit/file_sharing/ShowFiles.java
+++ app/src/main/java/com/dakshit/file_sharing/ShowFiles.java
@@ -1,33 +1,32 @@
 package com.dakshit.file_sharing;
 
 import android.app.ListActivity;
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ShowFiles extends ListActivity {
     protected File curDirectory;
     protected ArrayAdapter<String> listOfFileAdapter;
     private File root=new File(Environment.getExternalStorageDirectory().getAbsolutePath());
     private List<String> listOfFiles=new ArrayList<String>();
+    private Set<String> selectedFilesSet=new HashSet();
+    private TextView fileCountView;
     @Override
     protected void onCreate(Bundle savedInstanceState){
-        //TODO: add header with back & next button and title as file name
-        //TODO: add footer as no of selected file and nextbutton here(or in header)
         super.onCreate(savedInstanceState);
-        LayoutInflater inflator = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View emptyView = inflator.inflate(R.layout.filelist, null);
-        ((ViewGroup) getListView().getParent()).addView(emptyView);
-        getListView().setEmptyView(emptyView);
+        //LayoutInflater inflator = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        setContentView(R.layout.filelist);
+        fileCountView=findViewById(R.id.noSelectedFile);
         curDirectory=root;
         for(String listItem:curDirectory.list()){
             listOfFiles.add(listItem);
@@ -48,9 +47,15 @@ public class ShowFiles extends ListActivity {
         File selectedFile=new File(selectedFileName);
         if(selectedFile.isDirectory()){
             curDirectory=selectedFile;
+            selectedFilesSet.clear();
             refreshList();
         } else {
-            //TODO: add file into sending queue/array
+            if(selectedFilesSet.add(selectedFileName)){
+                fileCountView.setText(selectedFilesSet.size()+" files selected");
+            } else{
+                selectedFilesSet.remove(selectedFileName);
+                fileCountView.setText(selectedFilesSet.size()+" files selected");
+            }
         }
         super.onListItemClick(fileListView, view, position, id);
     }
@@ -58,8 +63,18 @@ public class ShowFiles extends ListActivity {
     @Override
     public void onBackPressed(){
         if(!curDirectory.equals(root)) {
-            curDirectory = curDirectory.getParentFile();
-            refreshList();
+            moveBack();
+        } else{
+            super.onBackPressed();
         }
+    }
+    public void goBackDir(View view){
+        if(!curDirectory.equals(root)) moveBack();
+    }
+    public void moveBack(){
+        curDirectory = curDirectory.getParentFile();
+        selectedFilesSet.clear();
+        fileCountView.setText(selectedFilesSet.size()+" files selected");
+        refreshList();
     }
 }

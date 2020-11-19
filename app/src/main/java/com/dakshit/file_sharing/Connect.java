e9c71446bdd69f95a296c3e872d31e6b38fb4928
Dakshit Mahadeshwar <41019458+dakshitgm@users.noreply.github.com>
2020-11-19 18:55:11 +0530

    nfile

---
+++ app/src/main/java/com/dakshit/file_sharing/Connect.java
@@ -0,0 +1,14 @@
+package com.dakshit.file_sharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Connect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+    }
+}

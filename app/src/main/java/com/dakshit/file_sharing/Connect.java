f515bade609600f50b9c9bfb8d2852301b49a741
Dakshit Mahadeshwar <41019458+dakshitgm@users.noreply.github.com>
2020-12-02 20:52:25 +0530

    to connect

--- app/src/main/java/com/dakshit/file_sharing/Connect.java
+++ app/src/main/java/com/dakshit/file_sharing/Connect.java
@@ -10,12 +10,20 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class Connect extends AppCompatActivity {
     private WifiP2pManager wifiP2pManager;
@@ -23,11 +31,19 @@ public class Connect extends AppCompatActivity {
     private WifiManager wifiManager;
     private BroadcastReceiver broadcastReceiver;
     private IntentFilter intentFilter;
+    private ListView peerList;
+    private Button retry;
+    private TextView message;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+
+        peerList = (ListView) findViewById(R.id.listPeers);
+        retry = (Button) findViewById(R.id.btnRetry);
+        message = (TextView) findViewById(R.id.tvMessage);
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         wifiManager.setWifiEnabled(true);
@@ -43,52 +59,47 @@ public class Connect extends AppCompatActivity {
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
 
-
         registerReceiver(broadcastReceiver, intentFilter);
-        discover();
+        discover(null);
     }
 
-    public void discover() {
+    public void discover(View view) {
+        if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
+            wifiManager.setWifiEnabled(true);
+        }
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
-            return;
         }
         wifiP2pManager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+
             @Override
             public void onSuccess() {
-                //TODO: some
+                message.setText("Sucsess");
             }
 
             @Override
             public void onFailure(int reason) {
-                //TODO: on fail
-            }
-        });
-        wifiP2pManager.requestPeers(channel, new WifiP2pManager.PeerListListener() {
-            @Override
-            public void onPeersAvailable(WifiP2pDeviceList peers) {
-                //Todo: inform user and let user choose
+                message.setText("searching fails, Retry");
             }
         });
     }
 
 
-        /*
+    public WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peers) {
+            List<String> deviceNameList = new ArrayList();
+            for (WifiP2pDevice device : peers.getDeviceList())
+                deviceNameList.add(device.deviceName);
 
-
-
-        HashSet selectedFileSet = (HashSet) getIntent().getSerializableExtra("fileList");
-        wifiP2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        //wifiP2pManager.initialize(this, getMainLooper(), null);
-        channel = wifiP2pManager.initialize(this, getMainLooper(), null);
-
-
-        */
-
-
+            listAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNameList);
+            peerList.setAdapter(listAdapter);
+            message.setText(String.valueOf(deviceNameList));
+        }
+    };
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         registerReceiver(broadcastReceiver, intentFilter);
     }
@@ -99,30 +110,33 @@ public class Connect extends AppCompatActivity {
         registerReceiver(broadcastReceiver, intentFilter);
     }
 }
-class WifiDirectBroadcastReceiver extends BroadcastReceiver{
+
+class WifiDirectBroadcastReceiver extends BroadcastReceiver {
     private WifiP2pManager wifiP2pManager;
     private WifiP2pManager.Channel channel;
     private Connect activity;
 
-    public WifiDirectBroadcastReceiver(WifiP2pManager wifiP2pManager, WifiP2pManager.Channel channel, Connect activity){
-        this.wifiP2pManager=wifiP2pManager;
-        this.channel=channel;
-        this.activity=activity;
+    public WifiDirectBroadcastReceiver(WifiP2pManager wifiP2pManager, WifiP2pManager.Channel channel, Connect activity) {
+        this.wifiP2pManager = wifiP2pManager;
+        this.channel = channel;
+        this.activity = activity;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        String action=intent.getAction();
-        if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)){
-            if(intent.getIntExtra(wifiP2pManager.EXTRA_WIFI_STATE, -1)==WifiP2pManager.WIFI_P2P_STATE_ENABLED){
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            if (intent.getIntExtra(wifiP2pManager.EXTRA_WIFI_STATE, -1) == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
                 Toast.makeText(context, "wifi is on", Toast.LENGTH_LONG).show();
-                activity.discover();
-            } else{
+            } else {
                 Toast.makeText(context, "wifi is off", Toast.LENGTH_LONG).show();
             }
 
-        }else if(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)){
-
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+            }
+            wifiP2pManager.requestPeers(channel, activity.peerListListener);
         }else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)){
 
         }else if(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)){

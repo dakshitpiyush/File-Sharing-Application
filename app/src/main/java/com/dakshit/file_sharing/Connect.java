5d7e6de29a82db519b7524eb9d2c9ac63020f101
Dakshit Mahadeshwar <dakshitdm@gmail.com>
2020-12-25 19:57:38 +0530

    used wifi-hotspot arch

--- app/src/main/java/com/dakshit/file_sharing/Connect.java
+++ app/src/main/java/com/dakshit/file_sharing/Connect.java
@@ -1,8 +1,5 @@
 package com.dakshit.file_sharing;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
@@ -10,177 +7,83 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.net.wifi.p2p.WifiP2pConfig;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pDeviceList;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import java.util.List;
 
 public class Connect extends AppCompatActivity {
-    private WifiP2pManager wifiP2pManager;
-    private WifiP2pManager.Channel channel;
     private WifiManager wifiManager;
-    private BroadcastReceiver broadcastReceiver;
-    private IntentFilter intentFilter;
-    private ListView peerList;
+    private WifiBroadcastReceiver wifiBroadcastReceiver;
+    private ListView listView;
     private Button retry;
-    public TextView message;
-    private ArrayAdapter<String> listAdapter;
-    public ArrayList<String> deviceList = new ArrayList<>();
+    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
 
-        peerList = (ListView) findViewById(R.id.listPeers);
+        listView = (ListView) findViewById(R.id.listPeers);
         retry = (Button) findViewById(R.id.btnRetry);
         message = (TextView) findViewById(R.id.tvMessage);
 
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        wifiManager.setWifiEnabled(true);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(this.WIFI_SERVICE);
+        wifiBroadcastReceiver = new WifiBroadcastReceiver(this);
 
-        wifiP2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = wifiP2pManager.initialize(this, getMainLooper(), null);
-        broadcastReceiver = new WifiDirectBroadcastReceiver(wifiP2pManager, channel, this);
-        intentFilter = new IntentFilter();
-
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-
-        registerReceiver(broadcastReceiver, intentFilter);
-        discover(null);
-    }
-
-    public void discover(View view) {
-        if (wifiManager.getWifiState() == WifiManager.WIFI_STATE_ENABLED) {
-            wifiManager.setWifiEnabled(true);
-        }
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        registerReceiver(wifiBroadcastReceiver, intentFilter);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
         }
-        wifiP2pManager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+        wifiManager.setWifiEnabled(true);
 
-            @Override
-            public void onSuccess() {
-                message.setText("Sucsess");
-            }
+        if (wifiManager.isWifiEnabled()) wifiManager.startScan();
 
-            @Override
-            public void onFailure(int reason) {
-                message.setText("searching fails, Retry");
-            }
-        });
-        peerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                WifiP2pConfig config = new WifiP2pConfig();
-                config.deviceAddress = deviceList.get(position);
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions((Activity) getApplicationContext(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
-                }
-                wifiP2pManager.connect(channel, config, new WifiP2pManager.ActionListener() {
-                    @Override
-                    public void onSuccess() {
-                        //message.setText("sucsess Fully connected to "+parent.getItemAtPosition(position));
-                    }
 
-                    @Override
-                    public void onFailure(int reason) {
-                        String getRes;
-                        if (reason == WifiP2pManager.P2P_UNSUPPORTED)
-                            getRes = "device is unsupported";
-                        else if (reason == wifiP2pManager.BUSY) getRes = "device is busy";
-                        else getRes = "unknown error occured try again";
-                        message.setText("Fail to connect device " + getRes);
-                    }
-                });
-
-            }
-        });
     }
 
-    public WifiP2pManager.PeerListListener peerListListener = new WifiP2pManager.PeerListListener() {
-        @Override
-        public void onPeersAvailable(WifiP2pDeviceList peers) {
-            List<String> deviceNameList = new ArrayList();
-            for (WifiP2pDevice device : peers.getDeviceList()) {
-                deviceNameList.add(device.deviceName);
-                deviceList.add(device.deviceAddress);
-            }
-
-            listAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNameList);
-            peerList.setAdapter(listAdapter);
-
-        }
-    };
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(broadcastReceiver, intentFilter);
+    public void onScanSuccess() {
+        message.setText("scanning sucsess");
+        List<ScanResult> deviceNameList = wifiManager.getScanResults();
+        ArrayAdapter<ScanResult> listAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNameList);
+        listView.setAdapter(listAdapter);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        registerReceiver(broadcastReceiver, intentFilter);
+    public void onFail() {
+        message.setText("scan fail kaytari karna mule");
     }
+
+
 }
 
-class WifiDirectBroadcastReceiver extends BroadcastReceiver {
-    private WifiP2pManager wifiP2pManager;
-    private WifiP2pManager.Channel channel;
-    private Connect activity;
+class WifiBroadcastReceiver extends BroadcastReceiver {
+    public Connect activity;
 
-    public WifiDirectBroadcastReceiver(WifiP2pManager wifiP2pManager, WifiP2pManager.Channel channel, Connect activity) {
-        this.wifiP2pManager = wifiP2pManager;
-        this.channel = channel;
+    public WifiBroadcastReceiver(Connect activity) {
         this.activity = activity;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
-            if (intent.getIntExtra(wifiP2pManager.EXTRA_WIFI_STATE, -1) == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                Toast.makeText(context, "wifi is on", Toast.LENGTH_LONG).show();
+        if (action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+            if (intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)) {
+                activity.onScanSuccess();
             } else {
-                Toast.makeText(context, "wifi is off", Toast.LENGTH_LONG).show();
+                activity.onFail();
             }
-
-        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-            if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
-            }
-            wifiP2pManager.requestPeers(channel, activity.peerListListener);
-        }else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
-            if (wifiP2pManager == null) return;
-            NetworkInfo networkInf = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
-            if (networkInf.isConnected()) {
-                activity.message.setText("connected");
-            } else {
-                activity.message.setText("Disconnected");
-            }
-
-        }else if(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)){
-
         }
+
     }
 }
--- app/src/main/java/com/dakshit/file_sharing/MainActivity.java
+++ app/src/main/java/com/dakshit/file_sharing/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dakshit.file_sharing;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -9,13 +10,22 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     private int STORAGE_PERMISSION_CODE = 1;
+    private WifiManager wifiManager;
+    private WifiManager.LocalOnlyHotspotReservation hotspotReservation;
+    private WifiConfiguration currentConfig;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +44,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void startHotspot(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 5);
+            return;
+        }
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(this.WIFI_SERVICE);
+        wifiManager.startLocalOnlyHotspot(new WifiManager.LocalOnlyHotspotCallback() {
+            @Override
+            public void onStarted(WifiManager.LocalOnlyHotspotReservation reservation) {
+                super.onStarted(reservation);
+                hotspotReservation = reservation;
+                currentConfig = hotspotReservation.getWifiConfiguration();
+
+                Log.v("DANG", "THE PASSWORD IS: "
+                        + currentConfig.preSharedKey
+                        + " \n SSID is : "
+                        + currentConfig.SSID);
+
+            }
+
+            @Override
+            public void onStopped() {
+                super.onStopped();
+                Log.v("DANG", "Local Hotspot Stopped");
+            }
+
+            @Override
+            public void onFailed(int reason) {
+                super.onFailed(reason);
+                Log.v("DANG", "Local Hotspot failed to start");
+            }
+
+        }, new Handler());
+    }
+
     //request for permissions
     private boolean requestPermission(String resource) {
         if (ContextCompat.checkSelfPermission(MainActivity.this, resource) == PackageManager.PERMISSION_DENIED) {
@@ -42,4 +88,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
 }

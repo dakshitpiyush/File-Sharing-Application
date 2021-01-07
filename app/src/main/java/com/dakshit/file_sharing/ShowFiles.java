5706a01b4934599d4ddc81224d6d8148012a87e8
piyushtakale <piyusht265@gmail.com>
2021-01-07 23:43:59 +0530

    yup

--- app/src/main/java/com/dakshit/file_sharing/ShowFiles.java
+++ app/src/main/java/com/dakshit/file_sharing/ShowFiles.java
@@ -1,61 +1,80 @@
 package com.dakshit.file_sharing;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class ShowFiles extends ListActivity {
     protected File curDirectory;
-    protected ArrayAdapter<String> listOfFileAdapter;
+    private TextView directoryPath;
+    protected MyListAdapter listOfFileAdapter;
     private File root;
-    private List<String> listOfFiles = new ArrayList<String>();
-    private ArrayList<String> selectedFilesSet = new ArrayList<>();
+    private ArrayList<File> listOfFiles = new ArrayList();
+    private HashSet<String> selectedFilesSet = new HashSet<>();
     private TextView fileCountView;
-
+    ArrayList<String > subs;
+    ArrayList<String> filenames=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filelist);
         fileCountView = findViewById(R.id.noSelectedFile);
         root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+        directoryPath=findViewById(R.id.dirName);
+
         curDirectory = root;
-        for (String listItem : curDirectory.list()) {
+
+        directoryPath.setText(curDirectory.getPath());
+
+        for (File listItem : curDirectory.listFiles()) {
             listOfFiles.add(listItem);
+            filenames.add(listItem.getName());
         }
-        listOfFileAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfFiles);
+        subs = new ArrayList<>();
+        for(int i = 0 ;i < curDirectory.length();i++){
+            subs.add("piyush");
+        }
+        listOfFileAdapter = new MyListAdapter(this, listOfFiles, subs, filenames);
         setListAdapter(listOfFileAdapter);
     }
     private void refreshList(){
         listOfFiles.clear();
-        for(String listItem:curDirectory.list()){
+        filenames.clear();
+        for(File listItem:curDirectory.listFiles()){
             listOfFiles.add(listItem);
+            filenames.add(listItem.getName());
         }
+        directoryPath.setText(curDirectory.getPath());
         listOfFileAdapter.notifyDataSetChanged();
     }
     @Override
     protected void onListItemClick(ListView fileListView, View view, int position, long id){
-        String selectedFileName=curDirectory+"/"+fileListView.getItemAtPosition(position);
-        File selectedFile=new File(selectedFileName);
+        File selectedFile=listOfFiles.get(position);
         if(selectedFile.isDirectory()){
             curDirectory=selectedFile;
             selectedFilesSet.clear();
             refreshList();
         } else {
-            if(selectedFilesSet.add(selectedFileName)){
+            if(selectedFilesSet.add(selectedFile.getAbsolutePath())){
                 fileCountView.setText(selectedFilesSet.size()+" files selected");
             } else{
-                selectedFilesSet.remove(selectedFileName);
+                selectedFilesSet.remove(selectedFile.getAbsolutePath());
                 fileCountView.setText(selectedFilesSet.size()+" files selected");
             }
         }
@@ -81,8 +100,83 @@ public class ShowFiles extends ListActivity {
     }
     public void connect(View view){
         Intent connect=new Intent(this, Connect.class);
-        connect.putExtra("fileList", selectedFilesSet);
+        ArrayList arr = new ArrayList(selectedFilesSet);
+        connect.putExtra("fileList", arr);
         startActivity(connect);
 
     }
 }
+
+class MyListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final ArrayList<File> maintitle;
+    private final ArrayList<String> subtitle;
+    private final ArrayList<String> filenames;
+    private final HashMap<String, Integer> icons = new HashMap(){{
+        put("png", R.drawable.image);
+        put("jpeg", R.drawable.image);
+        put("jpg", R.drawable.image);
+        put("svg", R.drawable.image);
+        put("gif", R.drawable.image);
+        put("mp4", R.drawable.video);
+        put("mpeg", R.drawable.video);
+        put("mkv", R.drawable.video);
+        put("avi", R.drawable.video);
+        put("flv", R.drawable.video);
+        put("wmv", R.drawable.video);
+        put("webm", R.drawable.video);
+        put("pdf", R.drawable.pdf);
+        put("doc", R.drawable.worddoc);
+        put("docx", R.drawable.worddoc);
+        put("xlsx", R.drawable.video);
+        put("txt", R.drawable.txt);
+        put("mp3", R.drawable.audio);
+        put("wav", R.drawable.audio);
+        put("m4a", R.drawable.audio);
+        put("zip", R.drawable.zip);
+        put("tar", R.drawable.zip);
+        put("rar", R.drawable.zip);
+    }};
+
+    public MyListAdapter(Activity context, ArrayList<File> maintitle,ArrayList<String> subtitle, ArrayList<String> filenames) {
+        super(context,R.layout.iconlist ,filenames);
+        // TODO Auto-generated constructor stub
+        this.context=context;
+        this.maintitle=maintitle;
+        this.subtitle=subtitle;
+        this.filenames = filenames;
+    }
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.iconlist, null,true);
+        TextView titleText = (TextView) rowView.findViewById(R.id.title);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        TextView subtitleText = (TextView) rowView.findViewById(R.id.subtitle);
+
+        titleText.setText(filenames.get(position));
+        if(maintitle.get(position).isDirectory()){
+            imageView.setImageResource(R.drawable.folder);
+        }
+        else{
+            int pos = maintitle.get(position).getName().lastIndexOf(".");
+            String filecha = maintitle.get(position).getName();
+            if(pos != -1){
+                imageView.setImageResource(icons.getOrDefault(maintitle.get(position).getName().substring(pos+1), R.drawable.unknown));
+            }
+            else {
+                imageView.setImageResource(R.drawable.unknown);
+            }
+        }
+
+        subtitleText.setText(subtitle.get(position));
+
+        return rowView;
+
+    };
+}
+
+/*
+
+ */

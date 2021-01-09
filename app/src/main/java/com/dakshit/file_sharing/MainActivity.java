e760f52c69bc2d001c4342e6fe2554bf3c21fcc4
Dakshit Mahadeshwar <dakshitdm@gmail.com>
2021-01-09 13:26:35 +0530

    diffrant sys for =<version Q

--- app/src/main/java/com/dakshit/file_sharing/MainActivity.java
+++ app/src/main/java/com/dakshit/file_sharing/MainActivity.java
@@ -1,23 +1,22 @@
 package com.dakshit.file_sharing;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,8 +38,19 @@ public class MainActivity extends AppCompatActivity {
     public void sendReceiveFile(View view) {
         if (requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
             if (view.getId() == R.id.btnSend) {
-                Intent selectFileIntent = new Intent(this, ShowFiles.class);
-                startActivity(selectFileIntent);
+                Intent selectFileIntent;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+
+                    selectFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    selectFileIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                    selectFileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                    selectFileIntent.setType("*/*");  //use image/* for photos, etc.
+                    startActivityForResult(selectFileIntent, 1001);
+
+                } else {
+                    selectFileIntent = new Intent(this, ShowFiles.class);
+                    startActivity(selectFileIntent);
+                }
             } else if (view.getId() == R.id.btnRecive) {
                 Intent receiveFileIntent = new Intent(this, Recieve.class);
                 startActivity(receiveFileIntent);
@@ -59,6 +69,29 @@ public class MainActivity extends AppCompatActivity {
             return ContextCompat.checkSelfPermission(MainActivity.this, resource) == PackageManager.PERMISSION_GRANTED;
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 1001 && data != null) {
+            ArrayList<String> selectedFileList = new ArrayList<>();
+            if (data.getClipData() != null) {
+                // Getting the length of data and logging up the logs using index
+                for (int index = 0; index < data.getClipData().getItemCount(); index++) {
+                    // Getting the URIs of the selected files and logging them into logcat at debug level
+                    Uri uri = data.getClipData().getItemAt(index).getUri();
+                    selectedFileList.add(FileUtils.getPath(getApplicationContext(), uri));
+                }
+            } else {
+                // Getting the URI of the selected file and logging into logcat at debug level
+                Uri uri = data.getData();
+                selectedFileList.add(FileUtils.getPath(getApplicationContext(), uri));
+            }
+            Intent connect = new Intent(this, Connect.class);
+            connect.putExtra("fileList", selectedFileList);
+            startActivity(connect);
+        }
     }
 
 }
